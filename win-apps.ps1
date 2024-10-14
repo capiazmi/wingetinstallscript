f56eac96a8864fff8bc04e998c3298a07bcb67a4
capiazmi <capi@live.com.my>
2024-10-14 13:04:21 +0800

    Update win-apps.ps1
    sorting

--- win-apps.ps1
+++ win-apps.ps1
@@ -20,29 +20,29 @@ function Install-IfNotInstalled {
 }
 
 # Install applications
-Install-IfNotInstalled 'dbeaver.dbeaverce' 'DBeaver'
-Install-IfNotInstalled 'Microsoft.VisualStudioCode' 'Visual Studio Code'
-Install-IfNotInstalled 'WinSCP.WinSCP' 'WinSCP'
-Install-IfNotInstalled 'Mobatek.MobaXterm' 'MobaXterm'
-Install-IfNotInstalled 'Notepad++.Notepad++' 'Notepad++'
-Install-IfNotInstalled 'Valve.Steam' 'Steam'
+Install-IfNotInstalled '7zip.7zip' '7-Zip'
 Install-IfNotInstalled 'Blizzard.BattleNet' 'Battle.net'
-Install-IfNotInstalled '9NKSQGP7F2NH' 'WhatsApp'
-Install-IfNotInstalled 'Bitwarden.Bitwarden' 'Bitwarden'
-Install-IfNotInstalled 'Kleopatra.Kleopatra' 'Kleopatra'
+Install-IfNotInstalled 'Bitdefender.Bitdefender' 'Bitdefender'
 Install-IfNotInstalled 'iterate.Cyberduck' 'Cyberduck'
-Install-IfNotInstalled 'Ubisoft.Uplay' 'Ubisoft Connect'
+Install-IfNotInstalled 'Discord.Discord' 'Discord'
+Install-IfNotInstalled 'dbeaver.dbeaverce' 'DBeaver'
 Install-IfNotInstalled 'ElectronicArts.EADesktop' 'EA app'
 Install-IfNotInstalled 'EpicGames.EpicGamesLauncher' 'Epic Games Launcher'
 Install-IfNotInstalled 'Google.Chrome' 'Google Chrome'
 Install-IfNotInstalled 'Google.Drive' 'Google Drive'
-Install-IfNotInstalled 'Discord.Discord' 'Discord'
-Install-IfNotInstalled '7zip.7zip' '7-Zip'
-Install-IfNotInstalled 'Transmission.TransmissionRemoteGui' 'Transmission Remote GUI'
-Install-IfNotInstalled 'Bitdefender.Bitdefender' 'Bitdefender'
 Install-IfNotInstalled 'CPUID.HWMonitor' 'HWMonitor'
+Install-IfNotInstalled 'Kleopatra.Kleopatra' 'Kleopatra'
 Install-IfNotInstalled 'Microsoft.Office' 'Microsoft Office 365'
+Install-IfNotInstalled 'Mobatek.MobaXterm' 'MobaXterm'
 Install-IfNotInstalled 'NVIDIACorporation.NVIDIAControlPanel' 'NVIDIA Drivers'
+Install-IfNotInstalled 'Notepad++.Notepad++' 'Notepad++'
+Install-IfNotInstalled 'Valve.Steam' 'Steam'
+Install-IfNotInstalled 'Transmission.TransmissionRemoteGui' 'Transmission Remote GUI'
+Install-IfNotInstalled 'Ubisoft.Uplay' 'Ubisoft Connect'
+Install-IfNotInstalled 'Microsoft.VisualStudioCode' 'Visual Studio Code'
+Install-IfNotInstalled '9NKSQGP7F2NH' 'WhatsApp'
+Install-IfNotInstalled 'WinSCP.WinSCP' 'WinSCP'
+
 
 
 
